92910dfe9302adef5773f6b53fd5d626a6d2cb6b
Nikk <nikita.lakhwani10@gmail.com>
2026-07-08 19:33:14 +0530

    Added datatypes and updated variables

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using node.js, not a browser so alert won't work
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("JavaScript") 
+
+let name = "John" 
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique identifier
+// object => { key: value } 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
--- variables.js
+++ variables.js
@@ -1,1 +1,20 @@
+const accountId = 144553
+let accountEmail = "user@example.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
 
+// accountId = 123456 // accountId is a constant and cannot be reassigned
+
+accountEmail = "newuser@example.com"
+accountPassword = "67890"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/* 
+Prefer not to use var
+because it has block scope and fuctional scope issues.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
