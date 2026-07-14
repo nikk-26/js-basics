af9fb3a69b11acf1f442563650afaa83460a0eac
Nikk <nikita.lakhwani10@gmail.com>
2026-07-14 19:52:57 +0530

    Add JavaScript Operations

--- operations.js
+++ operations.js
@@ -1,3 +1,38 @@
 let value = 3
 let negValue = -value
-console.log(negValue)
+//console.log(negValue)
+
+//console.log(3 + 2) 
+//console.log(3 - 2)
+//console.log(3 * 2)
+//console.log(3 / 2)
+//console.log(3 % 2)
+//console.log(3 ** 2) 
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log(3 + "2")
+// console.log("3" + 2)
+// console.log("3" + 2 + 2)
+// console.log(3 + 2 + "2")
+
+// console.log( (3 + 4) * 5 % 3 )
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+// Link to Study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
+// and
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
