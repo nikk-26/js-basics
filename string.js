239ae89a96ae8073f64e67d44a13deec69265063
Nikk <nikita.lakhwani10@gmail.com>
2026-07-24 17:30:11 +0530

    update string.js file

--- string.js
+++ string.js
@@ -1,4 +1,30 @@
 const name = "Nikk"
 const repoCount = 10
 
-console.log(name + repoCount + " Repositories") 
+// console.log(name + repoCount + " Repositories")
+
+console.log(`Hello ${name}, you have ${repoCount} repositories`)
+
+const gameName = new String(`nikk-nl-com`)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(7))
+// console.log(gameName.indexOf("n"))
+
+const newString = gameName.substring(0, 7)
+// console.log(newString)
+
+const anotherString = gameName.slice(-4, -1)
+// console.log(anotherString)
+
+const newStringOne = "    Nikk    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://nikk.nl.com/"
+
+// console.log(url.replace("https://", "http://"))
+// console.log(url.includes("nikk"))
+// console.log(gameName.split("-"))
